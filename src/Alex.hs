03fdb7d4b299c1b47d18631db95192dd0fc8ad71
Abo05 <alvaro.acedoblanco05@gmail.com>
2026-05-18 13:36:44 +0200

    Hecho el tipo Token y empezado a hacer la función de Alex

---
+++ src/Alex.hs
@@ -0,0 +1,69 @@
+module Alex where
+
+import Data.Char (isSpace, isDigit, isAlpha)
+
+-- Tipo algebraico que nos sirve para codificar los tipos de los tokens, haciendo que no se pase nada extraño
+data Code 
+    = TkEntero 
+    | TkReal
+    | TkCadena
+    | TkTrue
+    | TkFalse
+    | TkParentesisA
+    | TkParentesisC
+    | TkSuma
+    | TkResta
+    | TkMayor
+    | TkMayorIgual
+    | TkNot
+    | TkAnd
+    | TkAutodecremento
+    | TkAsignacion
+    | TkIdentificacion
+    | TkLet
+    | TkInt
+    | TkString
+    | TkBoolean
+    | TkFloat
+    | TkVoid
+    | TkPuntoComa
+    | TkLlaveA
+    | TkLlaveC
+    | TkIf
+    | TkElse
+    | TkComa
+    | TkFunction
+    | TkRead
+    | TkWrite
+    | TkEof
+
+-- Tipo algebraico para el lexema del token. Añadimos el lexema Nil para indicar que no tiene lexema ese token
+data Lex 
+    = Value
+    | Pos
+    | Lex
+    | Nil
+
+-- Definimos el tipo token como un codigo y un lexema
+type Token = (Code, Lex)
+
+getTokens :: String -> [Maybe Token]
+getTokens fich = getTokensAux fich 0
+    where
+        -- Caso de que el fichero ha acabado y lo ha hecho bien
+        getTokensAux "" 0 = [Just (TkEof, Nil)]
+        getTokensAux (c : r) 0
+            | isSpace c = getTokensAux r 0
+            | isDigit c = getTokensAux r 1
+            | (isAlpha c || c == '_') = getTokensAux r 4
+            | (c == '"') = getTokensAux r 5
+            | (c == '-') = getTokensAux r 7
+            | (c == '>') = getTokensAux r 8
+            | (c == '&') = getTokensAux r 9
+            | (c == '/') = getTokensAux r 10
+            | (c == ')') = Just (TkParentesisA, Nil) : getTokensAux r 0
+            | otherwise = Nothing : getTokensAux r 0 {- En caso de error devolvemos Nothing y volvemos al caso cero -}
+        -- Error pero todavia nos quedan caracteres que podemos leer, así que indicamos un error y continuamos
+        getTokensAux (_ : r) _ = Nothing : getTokensAux r 0
+        -- Error y no hay nada que leer, indicamos que ocurrio un error y que hemos leido el fin de fichero
+        getTokensAux "" _ = [Nothing, Just (TkEof, Nil)]
