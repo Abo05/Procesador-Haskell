30b26f4f6597983972f23873d7fbd4cd66f7e0f5
Abo05 <alvaro.acedoblanco05@gmail.com>
2026-05-28 12:22:32 +0200

    Cambiado a nombres significativos

--- src/Alex.hs
+++ src/Alex.hs
@@ -22,24 +22,24 @@ maxCadena = 64
 type Resultado = (Maybe Token, String, GError, TablaSimbolos, Bool)
 
 getToken :: String -> GError -> TablaSimbolos -> Resultado
-getToken fich ge ts = estado0 fich ge ts
+getToken fich ge ts = estadoIn fich ge ts
 
 ------------------------
 
 --Devuelvo el Token, gestor de errores, tabla de sismbolos y el string que queda después de haber leído el token
-estado0 :: String -> GError -> TablaSimbolos -> Resultado
-estado0 [] ge ts = (Just TkEof, [], ge, ts, False)
+estadoIn :: String -> GError -> TablaSimbolos -> Resultado
+estadoIn [] ge ts = (Just TkEof, [], ge, ts, False)
 
-estado0 (c:cs) ge ts
-                | c == '\n'             = estado0 cs (nuevaLinea ge) ts
-                | isSpace c             = estado0 cs ge ts
-                | isDigit c             = estado1 cs ge ts [c] (digitToInt c)
-                | isAlpha c || c == '_' = estado4 cs ge ts [c]
-                | c == '"'              = estado5 cs ge ts "" 0
-                | c == '-'              = estado7 cs ge ts
-                | c == '>'              = estado8 cs ge ts
-                | c == '&'              = estado9 cs ge ts
-                | c == '/'              = estado10 cs ge ts
+estadoIn (c:cs) ge ts
+                | c == '\n'             = estadoIn cs (nuevaLinea ge) ts
+                | isSpace c             = estadoIn cs ge ts
+                | isDigit c             = estadoNum cs ge ts [c] (digitToInt c)
+                | isAlpha c || c == '_' = estadoVar cs ge ts [c]
+                | c == '"'              = estadoCad cs ge ts "" 0
+                | c == '-'              = estadoMenos cs ge ts
+                | c == '>'              = estadoMayor cs ge ts
+                | c == '&'              = estadoAnd cs ge ts
+                | c == '/'              = estadoDiv cs ge ts
                 | c == '('  = (Just TkParentesisA,  cs, ge, ts, False)
                 | c == ')'  = (Just TkParentesisC,  cs, ge, ts, False)
                 | c == '+'  = (Just TkSuma,         cs, ge, ts, False)
@@ -58,12 +58,12 @@ estado0 (c:cs) ge ts
 -- Estados 1-10: todos devuelven False en el Bool salvo estado4
 -- (los demás no tocan la tabla de símbolos)
 
-estado1 :: String -> GError -> TablaSimbolos -> String -> Int -> Resultado
-estado1 [] ge ts _ ne =
+estadoNum :: String -> GError -> TablaSimbolos -> String -> Int -> Resultado
+estadoNum [] ge ts _ ne =
     (genEntero ge ne, [], ge, ts, False)
-estado1 (c:cs) ge ts lex ne
-    | isDigit c = estado1 cs ge ts (lex ++ [c]) (ne * 10 + digitToInt c)
-    | c == '.'  = estado2 cs ge ts ne
+estadoNum (c:cs) ge ts lex ne
+    | isDigit c = estadoNum cs ge ts (lex ++ [c]) (ne * 10 + digitToInt c)
+    | c == '.'  = estadoPuntoDec cs ge ts ne
     | otherwise = (genEntero ge ne, c:cs, ge, ts, False)
 
 genEntero :: GError -> Int -> Maybe Token
@@ -73,21 +73,21 @@ genEntero ge ne
 
 ------------------------------------------------------
 
-estado2 :: String -> GError -> TablaSimbolos -> Int -> Resultado
-estado2 [] ge ts _ =
+estadoPuntoDec :: String -> GError -> TablaSimbolos -> Int -> Resultado
+estadoPuntoDec [] ge ts _ =
     (Nothing, [], registrarError 2 ge, ts, False)
-estado2 (c:cs) ge ts ne
-    | isDigit c = estado3 cs ge ts ne (fromIntegral (digitToInt c)) 1
+estadoPuntoDec (c:cs) ge ts ne
+    | isDigit c = estadoReal cs ge ts ne (fromIntegral (digitToInt c)) 1
     | otherwise = (Nothing, c:cs, registrarError 2 ge, ts, False)
 
 ------------------------------------------------------
 
-estado3 :: String -> GError -> TablaSimbolos -> Int -> Float -> Int -> Resultado
-estado3 [] ge ts ne dec ndec =
+estadoReal :: String -> GError -> TablaSimbolos -> Int -> Float -> Int -> Resultado
+estadoReal [] ge ts ne dec ndec =
     let (tok, ge') = genReal ge ne dec ndec
     in (tok, [], ge', ts, False)
-estado3 (c:cs) ge ts ne dec ndec
-    | isDigit c = estado3 cs ge ts ne (dec * 10 + fromIntegral (digitToInt c)) (ndec + 1)
+estadoReal (c:cs) ge ts ne dec ndec
+    | isDigit c = estadoReal cs ge ts ne (dec * 10 + fromIntegral (digitToInt c)) (ndec + 1)
     | otherwise =
         let (tok, ge') = genReal ge ne dec ndec
         in (tok, c:cs, ge', ts, False)
@@ -102,10 +102,10 @@ genReal ge ne dec ndec =
 ------------------------------------------------------
 
 -- Estado 4: único que puede insertar en la tabla de símbolos
-estado4 :: String -> GError -> TablaSimbolos -> String -> Resultado
-estado4 [] ge ts lex = resolverIdent [] ge ts lex
-estado4 (c:cs) ge ts lex
-    | isAlphaNum c || c == '_' = estado4 cs ge ts (lex ++ [c])
+estadoVar :: String -> GError -> TablaSimbolos -> String -> Resultado
+estadoVar [] ge ts lex = resolverIdent [] ge ts lex
+estadoVar (c:cs) ge ts lex
+    | isAlphaNum c || c == '_' = estadoVar cs ge ts (lex ++ [c])
     | otherwise                = resolverIdent (c:cs) ge ts lex
 
 resolverIdent :: String -> GError -> TablaSimbolos -> String -> Resultado
@@ -121,69 +121,69 @@ resolverIdent resto ge ts lex =
 
 ------------------------------------------------------
 
-estado5 :: String -> GError -> TablaSimbolos -> String -> Int -> Resultado
-estado5 [] ge ts _ _ =
+estadoCad :: String -> GError -> TablaSimbolos -> String -> Int -> Resultado
+estadoCad [] ge ts _ _ =
     (Nothing, [], registrarError 5 ge, ts, False)
-estado5 (c:cs) ge ts lex cont
+estadoCad (c:cs) ge ts lex cont
     | c == '"'  =
         if cont >= maxCadena
             then (Nothing, cs, registrarError 28 ge, ts, False)
             else (Just (TkCadena lex), cs, ge, ts, False)
-    | c == '\\' = estado6 cs ge ts lex cont
+    | c == '\\' = estadoCarEsp cs ge ts lex cont
     | c == '\t' = (Nothing, cs, registrarError 4 ge, ts, False)
     | c == '\n' = (Nothing, cs, registrarError 5 (nuevaLinea ge), ts, False)
-    | otherwise = estado5 cs ge ts (lex ++ [c]) (cont + 1)
+    | otherwise = estadoCad cs ge ts (lex ++ [c]) (cont + 1)
 
 ------------------------------------------------------
 
-estado6 :: String -> GError -> TablaSimbolos -> String -> Int -> Resultado
-estado6 [] ge ts _ _ =
+estadoCarEsp :: String -> GError -> TablaSimbolos -> String -> Int -> Resultado
+estadoCarEsp [] ge ts _ _ =
     (Nothing, [], registrarError 6 ge, ts, False)
-estado6 (c:cs) ge ts lex cont
-    | c == 'n'  = estado5 cs ge ts (lex ++ "\n") (cont + 1)
-    | c == 't'  = estado5 cs ge ts (lex ++ "\t") (cont + 1)
+estadoCarEsp (c:cs) ge ts lex cont
+    | c == 'n'  = estadoCad cs ge ts (lex ++ "\n") (cont + 1)
+    | c == 't'  = estadoCad cs ge ts (lex ++ "\t") (cont + 1)
     | otherwise = (Nothing, c:cs, registrarError 6 ge, ts, False)
 
 ------------------------------------------------------
 
-estado7 :: String -> GError -> TablaSimbolos -> Resultado
-estado7 [] ge ts = (Just TkResta, [], ge, ts, False)
-estado7 (c:cs) ge ts
+estadoMenos :: String -> GError -> TablaSimbolos -> Resultado
+estadoMenos [] ge ts = (Just TkResta, [], ge, ts, False)
+estadoMenos (c:cs) ge ts
     | c == '-'  = (Just TkAutodecremento, cs, ge, ts, False)
     | otherwise = (Just TkResta, c:cs, ge, ts, False)
 
 ------------------------------------------------------
 
-estado8 :: String -> GError -> TablaSimbolos -> Resultado
-estado8 [] ge ts = (Just TkMayor, [], ge, ts, False)
-estado8 (c:cs) ge ts
+estadoMayor :: String -> GError -> TablaSimbolos -> Resultado
+estadoMayor [] ge ts = (Just TkMayor, [], ge, ts, False)
+estadoMayor (c:cs) ge ts
     | c == '='  = (Just TkMayorIgual, cs, ge, ts, False)
     | otherwise = (Just TkMayor, c:cs, ge, ts, False)
 
 ------------------------------------------------------
 
-estado9 :: String -> GError -> TablaSimbolos -> Resultado
-estado9 [] ge ts =
+estadoAnd :: String -> GError -> TablaSimbolos -> Resultado
+estadoAnd [] ge ts =
     (Nothing, [], registrarError 9 ge, ts, False)
-estado9 (c:cs) ge ts
+estadoAnd (c:cs) ge ts
     | c == '&'  = (Just TkAnd, cs, ge, ts, False)
     | otherwise = (Nothing, c:cs, registrarError 9 ge, ts, False)
 
 ------------------------------------------------------
 
-estado10 :: String -> GError -> TablaSimbolos -> Resultado
-estado10 [] ge ts =
+estadoDiv :: String -> GError -> TablaSimbolos -> Resultado
+estadoDiv [] ge ts =
     (Nothing, [], registrarError 10 ge, ts, False)
-estado10 (c:cs) ge ts
-    | c == '/'  = estado11 cs ge ts
+estadoDiv (c:cs) ge ts
+    | c == '/'  = estadoComentario cs ge ts
     | otherwise = (Nothing, c:cs, registrarError 10 ge, ts, False)
 
 ------------------------------------------------------
 
-estado11 :: String -> GError -> TablaSimbolos -> Resultado
-estado11 [] ge ts = (Just TkEof, [], ge, ts, False)
-estado11 (c:cs) ge ts
-    | c == '\n' = estado0 cs (nuevaLinea ge) ts
-    | otherwise = estado11 cs ge ts
+estadoComentario :: String -> GError -> TablaSimbolos -> Resultado
+estadoComentario [] ge ts = (Just TkEof, [], ge, ts, False)
+estadoComentario (c:cs) ge ts
+    | c == '\n' = estadoIn cs (nuevaLinea ge) ts
+    | otherwise = estadoComentario cs ge ts
 ------------------------------------------------------
 
